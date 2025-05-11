e24b96c769878ddc944cbbc8a7eb779b94f5c172
jonas-yukins <jmy2133@columbia.edu>
2025-05-11 17:50:26 -0400

    Initial Commit

---
+++ Sift Figma Prototype/Sift_Figma_PrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sift_Figma_PrototypeApp.swift
+//  Sift Figma Prototype
+//
+//  Created by Jonas Yukins on 5/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Sift_Figma_PrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
